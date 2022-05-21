a3779fb5a998bebe8a4494969f036e4d7189b18a
hmedasani <h.medasani@gmail.com>
2022-05-22 05:02:54 +0530

    refactor: adding mui styled

--- app/atoms/form/button_menu/index.tsx
+++ app/atoms/form/button_menu/index.tsx
@@ -1,8 +1,14 @@
+import { styled } from '@mui/material';
 import Button, { ButtonProps } from '@mui/material/Button';
 import React from 'react';
 
+const ButtonNew = styled(Button)`
+  background-color: red;
+  color: white;
+`;
+
 function ButtonMenu(props: ButtonProps) {
-  return <Button>{props.children}</Button>;
+  return <ButtonNew>{props.children}</ButtonNew>;
 }
 
 export default ButtonMenu;
